81743de12e1aec5366f1a4cbe05c136428c9d433
baigali <baigalikkk@163.com>
2018-08-14 20:45:56 +0800

    dd function add called file name and line number

--- helpers.php
+++ helpers.php
@@ -529,6 +529,10 @@ if (! function_exists('dd')) {
      */
     function dd(...$args)
     {
+        $trace = debug_backtrace();
+
+        $args = array_merge([$trace[0]['file'].':'.$trace[0]['line']], $args);
+        
         foreach ($args as $x) {
             (new Dumper)->dump($x);
         }
